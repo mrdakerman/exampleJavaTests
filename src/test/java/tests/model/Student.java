dc26646f7abb6e6b121a6c6502fb253c05bed2d7
Dekker <ddyack@yandex.ru>
2025-06-04 08:51:50 +0300

    add lombok to Student model

--- src/test/java/tests/model/Student.java
+++ src/test/java/tests/model/Student.java
@@ -2,50 +2,20 @@ package tests.model;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Student {
     private Long id;
     private String name;
     private int age;
     private List<String> subjects;
-
-    public Student() {
-    }
-
-    public Student(String name, int age, List<String> subjects) {
-        this.name = name;
-        this.age = age;
-        this.subjects = subjects;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public List<String> getSubjects() {
-        return subjects;
-    }
-
-    public void setSubjects(List<String> subjects) {
-        this.subjects = subjects;
-    }
-} 
+}
