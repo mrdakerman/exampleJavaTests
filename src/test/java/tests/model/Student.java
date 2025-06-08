e6ab1716a1b420b3d21df617eced79fd28bd1fee
Dekker <ddyack@yandex.ru>
2025-06-08 13:45:08 +0300

    add new constructor for Student.java

--- src/test/java/tests/model/Student.java
+++ src/test/java/tests/model/Student.java
@@ -2,20 +2,24 @@ package tests.model;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class Student {
     private Long id;
     private String name;
     private int age;
     private List<String> subjects;
+
+    public Student(String name, int age, List<String> subjects) {
+        this.name = name;
+        this.age = age;
+        this.subjects = subjects;
+    }
+
 }
